d8fd16e45b0d1d1095692682b23d294c936ed3a2
midopooler <midhapulkit28@gmail.com>
2020-08-31 15:45:22 +0530

    fds
    fds

--- Assets/Plugins/Bugsee/BugseePluginVersion.cs
+++
@@ -1,8 +0,0 @@
-namespace BugseePlugin
-{
-	public static class BugseePluginVersion 
-	{
-		public const string BUGSEE_PLUGIN_VERSION = "1.7.3";
-		public const string BUGSEE_PLUGIN_BUILD = "a57f789-";
-	}
-}
